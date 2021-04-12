606341fb2252c00297046e9ffacba82474962462
Yahya1547 <1547yahya@gmail.com>
2021-04-12 16:32:19 +0700

    setup bottom navigation

--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.workoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.workoutapp.databinding.ActivityMainBinding
+import com.example.workoutapp.news.NewsFragment
+import com.example.workoutapp.tracker.TrackerFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.news_page -> {
+                val newsFragment = NewsFragment.newInstance()
+                openFragment(newsFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.tracker_page -> {
+                val trackerFragment = TrackerFragment.newInstance()
+                openFragment(trackerFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.history_page -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.scheduler_page -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            else -> return@OnNavigationItemSelectedListener false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
