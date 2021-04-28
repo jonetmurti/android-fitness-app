f1156612418552c76948f6ea5874f8f481656c2e
jonetmurti <composer202j@gmail.com>
2021-04-28 19:27:49 +0700

    implement calendar fragment

---
+++ app/src/main/java/com/example/workoutapp/history/CalendarFragment.kt
@@ -0,0 +1,101 @@
+package com.example.workoutapp.history
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.workoutapp.R
+import com.example.workoutapp.databinding.FragmentCalendarBinding
+import java.time.LocalDate
+import java.util.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CalendarFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CalendarFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var _binding: FragmentCalendarBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var logListButton: Button
+    private lateinit var calendarView: CalendarView
+    private lateinit var date: LocalDate
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val calendar: Calendar = Calendar.getInstance()
+        date = LocalDate.of(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        calendarView = view.findViewById(R.id.calendarView)
+        calendarView.setOnDateChangeListener { _, year: Int, month: Int, day: Int  ->
+            date = LocalDate.of(year, month + 1, day)
+        }
+
+        logListButton = binding.logListBtn
+        logListButton.setOnClickListener {
+            findNavController()
+                .navigate(CalendarFragmentDirections.actionHistoryPageToLogListPage(date))
+        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CalendarFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+                CalendarFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar(){
+        val navController = findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar
+            .setupWithNavController(navController, appBarConfiguration)
+    }
+}
