ed8021519f1df1b50c3d71987842f88a77b772cc
jonetmurti <composer202j@gmail.com>
2021-04-30 23:25:56 +0700

    show map in cycling detail

--- app/src/main/java/com/example/workoutapp/history/CyclingDetailFragment.kt
+++ app/src/main/java/com/example/workoutapp/history/CyclingDetailFragment.kt
@@ -1,11 +1,12 @@
 package com.example.workoutapp.history
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -13,8 +14,13 @@ import com.example.workoutapp.R
 import com.example.workoutapp.database.TrackerDao
 import com.example.workoutapp.database.TrainingDatabase
 import com.example.workoutapp.databinding.FragmentCyclingDetailBinding
-import java.util.*
 import com.example.workoutapp.service.calculateTotalDistance
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,12 +32,20 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CyclingDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CyclingDetailFragment : Fragment() {
+class CyclingDetailFragment : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var _binding: FragmentCyclingDetailBinding? = null
     private val binding get() = _binding!!
+
+    private var _mMap: GoogleMap? = null
+    private val mMap get() = _mMap!!
+
+    private var _polyline: Polyline? = null
+    private val polyline get() = _polyline!!
+
+    private var listLatLng: List<LatLng> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -113,5 +127,20 @@ class CyclingDetailFragment : Fragment() {
 
         binding.toolbar
             .setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        _mMap = p0;
+        val googlePlex = CameraPosition.Builder()
+                .target(LatLng(22.7739, 71.6673))
+                .zoom(7f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+        Log.d("Tracker Result Fragment", listLatLng.toString())
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 5000, null)
+        _polyline = mMap.addPolyline(PolylineOptions()
+                .addAll(listLatLng))
     }
 }
